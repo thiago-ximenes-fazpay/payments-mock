7edd1982f55a436b758798c3e67fca12edcccf1e
neijunior <nei.junior@fazpay.io>
2025-01-01 23:03:29 -0300

    fix: to found boleto by line or by bar code

--- src/app/api/pagamentosIB/api/v7/ContasCorrentes/00019/0000263335/Transacoes/ConsultarDadosDoBoleto/route.ts
+++ src/app/api/pagamentosIB/api/v7/ContasCorrentes/00019/0000263335/Transacoes/ConsultarDadosDoBoleto/route.ts
@@ -1,36 +1,43 @@
-import { getBoletoByLine } from "@/app/actions";
+import { getBoletoByBarCode, getBoletoByLine } from "@/app/actions";
 import { HttpStatus } from "@/constants/HttpStatus";
 import Boleto from "@/server/db/boleto.model";
 import { hasRendimentoToken } from "@/utils/rendimento-token.middleware";
 import { NextRequest, NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export async function POST(req: NextRequest) {
-  if (!hasRendimentoToken(req.headers.get('access_token')!)) {
-    return NextResponse.json({
-      error: 'Unauthorized',
-    },
+  if (!hasRendimentoToken(req.headers.get("access_token")!)) {
+    return NextResponse.json(
+      {
+        error: "Unauthorized",
+      },
       {
         status: HttpStatus.UNAUTHORIZED,
-      })
+      }
+    );
   }
 
-const body = await req.json();
+  const body = await req.json();
 
   const line = body.linhaDigitavel;
 
   const boleto = await getBoletoByLine(line);
 
-  if (!boleto) {
-    return NextResponse.json({
-      error: 'Boleto not found',
-    }, {
-      status: HttpStatus.NOT_FOUND,
-    })
+  const boletoByBarCode = await getBoletoByBarCode(line);
+
+  if (!boleto && !boletoByBarCode) {
+    return NextResponse.json(
+      {
+        error: "Boleto not found",
+      },
+      {
+        status: HttpStatus.NOT_FOUND,
+      }
+    );
   }
 
   return NextResponse.json({
-    value: boleto,
-  })
+    value: boleto || boletoByBarCode,
+  });
 }
